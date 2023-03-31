237893a100a1e3906fac523075efe048eaf586fa
laon0125 <jinu0125@gmail.com>
2023-03-31 13:58:33 +0900

    see my friend list

--- src/main/java/com/hanwhee/friendservice/repository/FriendRepository.java
+++ src/main/java/com/hanwhee/friendservice/repository/FriendRepository.java
@@ -1,8 +1,14 @@
 package com.hanwhee.friendservice.repository;
 
+import com.hanwhee.friendservice.domain.dto.FriendDTO;
 import com.hanwhee.friendservice.domain.entity.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+    List<Friend> findAllByMyId(Long myId);
+    Optional<Friend> findOneByMyId(Long myId);
 
 }
--- src/main/java/com/hanwhee/friendservice/service/FriendService.java
+++ src/main/java/com/hanwhee/friendservice/service/FriendService.java
@@ -3,11 +3,16 @@ package com.hanwhee.friendservice.service;
 import com.hanwhee.friendservice.domain.dto.FriendDTO;
 import com.hanwhee.friendservice.domain.entity.Friend;
 import com.hanwhee.friendservice.domain.request.InsertFriendRequest;
+import com.hanwhee.friendservice.domain.respose.ViewAllFriendList;
 import com.hanwhee.friendservice.repository.FriendRepository;
 import com.hanwhee.friendservice.security.SecurityService;
 import com.hanwhee.friendservice.security.TokenInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +24,17 @@ public class FriendService {
         Friend friend = new Friend(dto);
         friendRepository.save(friend);
         return "친구추가완료";
+    }
+
+    public List<ViewAllFriendList> findMyAllFriend(Long myId) {
+        List<ViewAllFriendList> viewAllFriendLists = new ArrayList<>();
+        List<Friend> all = friendRepository.findAllByMyId(myId);
+        for (Friend one : all) {
+            viewAllFriendLists.add(new ViewAllFriendList(
+                    one.getMyId(),
+                    one.getFriendId()
+            ));
+        }
+        return viewAllFriendLists;
     }
 }
