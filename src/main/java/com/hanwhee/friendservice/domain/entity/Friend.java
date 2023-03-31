b2df617b13c7eb3f210e67766311781949e7edfa
youruchul <youruchul@naver.com>
2023-03-31 10:47:13 +0900

    entity Friend

--- src/main/java/com/hanwhee/friendservice/domain/entity/Friend.java
+++ src/main/java/com/hanwhee/friendservice/domain/entity/Friend.java
@@ -1,4 +1,22 @@
 package com.hanwhee.friendservice.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity @Getter
+@AllArgsConstructor @NoArgsConstructor
+
 public class Friend {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long myId;
+    private Long friendId;
+    private boolean isFriend;
+
 }
